011ebf36ca3c4a3ff0d2f4adf8a673a8df89d427
twij <30731724+twij@users.noreply.github.com>
2020-04-22 20:47:02 +0100

    initial profile support

--- yeetbulb.py
+++ yeetbulb.py
@@ -1,4 +1,5 @@
 from yeelight import Bulb
+import json
 
 class YeetBulb:
     def __init__(self, name, ip):
@@ -6,7 +7,7 @@ class YeetBulb:
         self.ip = ip
         self.bulb = Bulb(self.ip, effect="smooth", duration=750, auto_on=True)
         
-    def __getitem__(self,key):
+    def __getitem__(self, key):
         return getattr(self, key)
 
     def turnoff(self):
@@ -15,12 +16,32 @@ class YeetBulb:
     def turnon(self):
         self.bulb.turn_on()
 
-    def day(self):
-        self.bulb.set_brightness(100)
-        self.bulb.set_color_temp(4700)
-        self.bulb.set_default()
+    def loadProfile(self, profile):
+        with open('profiles/' + profile + '.json', 'r') as f:
+            config = json.load(f)
+        
+        profile = config['profile']
+        
+        print('Loading ' + profile['name'])
 
-    def night(self):
-        self.bulb.set_brightness(5)
-        self.bulb.set_color_temp(2700)
-        self.bulb.set_default()
+        if ('rgb' in profile):
+            print('Setting RGB: ' + str(profile['rgb']))
+            self.bulb.set_rgb(profile['rgb'][0], profile['rgb'][1], profile['rgb'][2])
+
+        if('hsv' in profile):
+            print('Setting HSV: ' + str(profile['hsv']))
+            self.bulb.set_hsv(profile['hsv'][0], profile['hsv'][1], profile['hsv'][2])
+        
+        if ('color_temp' in profile):
+            print('Setting Colour Temp: ' + str(profile['color_temp']))
+            self.bulb.set_color_temp(profile['color_temp'])
+        
+        if ('brightness' in profile):
+            print('Setting Brightness: ' + str(profile['brightness']))
+            self.bulb.set_brightness(profile['brightness'])
+
+        if ('color_temp' in profile):
+            print('Setting Colour Temp: ' + str(profile['color_temp']))
+            self.bulb.set_color_temp(profile['color_temp'])
+        
+        #self.bulb.set_default()
--- yeetlight.py
+++ yeetlight.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 from tkinter import *
 from yeelight import Bulb
-from yeetbulb import YeetBulb
+from functools import partial
 import json
+import os
+from yeetbulb import YeetBulb
 
 current = 0
 
@@ -10,10 +12,8 @@ def turnon():
     bulbs[current].turnon()
 def turnoff():
     bulbs[current].turnoff()
-def day():
-    bulbs[current].day()
-def night():
-    bulbs[current].night()
+def loadProfile(profile):
+    bulbs[current].loadProfile(profile)
 
 #load config
 with open('config.json', 'r') as f:
@@ -23,6 +23,8 @@ bulbs = []
 for bulb in config['bulbs']:
     bulbs.append(YeetBulb(bulb['name'], bulb['ip']))
 
+buttons = config['buttons']
+
 #create window
 yeetlight = Tk()
 yeetlight.title('Yeetlight')
@@ -30,28 +32,41 @@ topFrame = Frame(yeetlight)
 topFrame.pack()
 
 #create buttons
-buttonOn = Button(topFrame, text="On", bg="black", fg="green", command=turnon)
-buttonOn.pack(side=LEFT)
-buttonOff = Button(topFrame, text="Off", bg="black", fg="red", command=turnoff)
-buttonOff.pack(side=LEFT)
-buttonDaylight = Button(topFrame, text="Daylight bright", bg="black", fg="gold", command=day)
-buttonDaylight.pack(side=LEFT)
-buttonNightmode = Button(topFrame, text="Night mode", bg="black", fg="blue", command=night)
-buttonNightmode.pack(side=LEFT)
+if buttons['on']:
+    Button(topFrame, text="On", bg="black", fg="green", command=turnon).pack(side=LEFT)
+if buttons['off']:
+    Button(topFrame, text="Off", bg="black", fg="red", command=turnoff).pack(side=LEFT)
+for button in buttons['custom']:
+    tbtn = Button(topFrame, text=button['name'], bg=button['bg'], fg=button['fg'], command=partial(loadProfile, button['profile']))
+    tbtn.pack(side=LEFT)
+
+#create profile dropdown
+profileSelect = StringVar(yeetlight)
+profiles = []
+
+for file in os.listdir("./profiles"):
+    if file.endswith(".json"):
+        profiles.append(file.rsplit( ".", 1 )[ 0 ])
+
+OptionMenu(topFrame, profileSelect, *profiles).pack()
+profileSelect.set('Profile')
+
+def set_profile(*args):
+    loadProfile(profileSelect.get())
+
+profileSelect.trace('w', set_profile)
 
 #create light dropdown
 tkvar = StringVar(yeetlight)
 choices = [x["name"] for x in bulbs]
-choices = dict(zip(choices,range(len(choices))))
+choices = dict(zip(choices, range(len(choices))))
 OptionMenu(topFrame, tkvar, *choices.keys()).pack()
-tkvar.set('Select')
+tkvar.set('Bulb')
 
-# on change dropdown value
 def change_dropdown(*args):
     global current
     current = choices[tkvar.get()]
 
-# link function to change dropdown
 tkvar.trace('w', change_dropdown)
 
 yeetlight.call('wm', 'attributes', '.', '-topmost', '1') # Force on top of other windows, delete if you wish
